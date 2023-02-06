4013b4e456e595f317d1cfc55199c791625236a3
Anastasia Okhapkina <praktikant@validdata.de>
2023-02-06 09:35:45 +0100

    Some Css / pending CartItems DB impl

--- src/routes/create-customer/+page.svelte
+++ src/routes/create-customer/+page.svelte
@@ -28,47 +28,47 @@ error: {form?.error}
 
         <div class = "bar" style="border-top: 1px #e9e9e9 solid;"> 
             <p class = "p">Name:</p>
-            
-            <input name="name" />
-            <button>submit</button>
+            <input class = "input" name="name" />
+
         </div>    
 
         <div class = "bar" >
             <p class = "p">Surname:</p>
+            <input class = "input" name = "surname" />
 
-            <input name = "surname" />
-            <button>submit</button>
         </div> 
 
         <div class = "bar" > 
             <p class = "p">City:</p>
-            <input name = "city" />
-            <button>submit</button>
+            <input class = "input" name = "city" />
+
         </div> 
 
         <div class = "bar" > 
             <p class = "p">Post code:</p>
-            <input name="postcode" />
-            <button>submit</button>
+            <input class = "input" name="postcode" />
+
         </div> 
 
         <div class = "bar" > 
             <p class = "p">Adress:</p>
-            <input name="adress" />
-            <button>submit</button>
+            <input class = "input" name="adress" />
+
         </div> 
 
         <div class = "bar" > 
             <p class = "p">E-mail:</p>
-            <input name="email" />
-            <button>submit</button>
+            <input class = "input" name="email" />
+
         </div> 
 
-        <div class = "bar" style="border-bottom: 1px #e9e9e9 solid;"> 
+        <div class = "bar"> 
             <p class = "p">Password:</p>
-            <input name="password" />
-            <button>submit</button>
+            <input class = "input" name="password" />
+            
         </div> 
+
+        <button class="button">Submit</button>
 
     </form>
 
@@ -106,6 +106,36 @@ error: {form?.error}
     .text {
         place-self: center;
         margin: 15px;
+    }
+
+    .button {
+        border: hidden;
+        padding: 10px;
+        width: 100%;
+        place-self: center;
+        border-radius: 5px;
+        background-color: #a8dadc;
+        color: #1e5670;
+    }
+    .button:hover {
+        cursor: pointer;
+        background-color: rgb(170, 227, 230);
+
+    }
+
+    button:active {
+        cursor: pointer;
+        background-color: #7cadc4;
+    }
+
+    .input{
+        border-radius: 3px;
+        border: 1px #ccc solid;
+
+    }
+
+    .input:hover {
+        background-color: #f7f7f7;
     }
 
 
--- src/routes/login-customer/+page.svelte
+++ src/routes/login-customer/+page.svelte
@@ -30,22 +30,24 @@ errorPass: {form?.errorPass}
     <p class = "p" style="place-self: center ;  color:green">Log in successful</p>
     {/if}
 
-    <form method="post">
+    <form  method="post">
 
     <div class = "bar" style =" border-top: 1px #e9e9e9 solid;"> 
         <p class = "p">E-mail:</p>
-        <input name="email" />
-        <button>submit</button>
+        <input class = "input" name="email" />
+
     </div> 
 
-    <div class = "bar" style =" border-bottom: 1px #e9e9e9 solid;"> 
+    <div class = "bar" > 
         <p class = "p">Password:</p>
-        <input name="password" />
+        <input class = "input" name="password" />
         
-        <button>submit</button>
+
     </div> 
 
+        <button class="button">Submit</button>
     </form>
+
 
     <div class = "text">Don't have account yet?</div>
     <a href="./create-customer" class = "text" style=" margin: 5px;">
@@ -84,6 +86,37 @@ errorPass: {form?.errorPass}
     .text {
         place-self: center;
         margin: 15px;
+    }
+
+
+    .button {
+        border: hidden;
+        padding: 10px;
+        width: 100%;
+        place-self: center;
+        border-radius: 5px;
+        background-color: #a8dadc;
+        color: #1e5670;
+    }
+    .button:hover {
+        cursor: pointer;
+        background-color: rgb(170, 227, 230);
+
+    }
+
+    button:active {
+        cursor: pointer;
+        background-color: #7cadc4;
+    }
+
+    .input{
+        border-radius: 3px;
+        border: 1px #ccc solid;
+
+    }
+
+    .input:hover {
+        background-color: #f7f7f7;
     }
 
 </style>
